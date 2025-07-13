fc87e359e3cb2f5c941dc5293106f3732848c60f
Sanika Sangappa Kamble <sanikak691@gmail.com>
2025-07-13 20:30:22 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+ import Ipl from "./Ipl"
+function App() 
+{
+  
+  return (
+    <div style={{"background":"pink" ,"textAlign":"center" ,"position":"absolute"}}>
+     <Ipl/> 
+  
+    </div>
+  )
+}
+
+export default App;
